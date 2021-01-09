6dded6183cf9d821b6771710134649b93dba305c
Sandeep Kumar <tsksandeep11@gmail.com>
2021-01-09 16:18:34 +0530

    Minor change

--- src/components/Education/Education.js
+++ src/components/Education/Education.js
@@ -16,7 +16,7 @@ const Education = () => {
                                 <h3 className="role">Bachelors of Engineering, Electronics and Communication</h3>
                                 <h4 className="organization">SSN College of Engineering</h4>
                             </div>
-                            <div className="col-6"><span className="period">2016 - 2020</span></div>
+                            <div className="col-6 custom-period"><span className="period">2016 - 2020</span></div>
                         </div>
                         <p className="text-muted role-description">• Coursework: Networking, FPGA Programming, Digital Electronics, Digital
                         Signal Processing, Antennas and Wave Propagation.<br /> <br /> • CGPA: 7.4 / 10</p>
@@ -27,7 +27,7 @@ const Education = () => {
                                 <h3 className="role">Science Group</h3>
                                 <h4 className="organization">Holy Cross School</h4>
                             </div>
-                            <div className="col-md-6"><span className="period">2003 - 2016</span></div>
+                            <div className="col-md-6 custom-period"><span className="period">2003 - 2016</span></div>
                         </div>
                         <p className="text-muted role-description">• Coursework: Biology, Maths, Physics, Chemistry. <br /><br />• Percentage: 94%</p>
                     </div>
