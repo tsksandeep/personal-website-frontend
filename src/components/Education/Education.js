ec6113b997856eb59fc78e88d623b5d64e50daf8
tsksandeep <tsksandeep11@gmail.com>
2021-11-07 18:33:10 +0530

    Updated with new details

--- src/components/Education/Education.js
+++ src/components/Education/Education.js
@@ -3,38 +3,56 @@ import "./Education.css";
 import "../../assets/bootstrap/css/bootstrap.min.css";
 
 const Education = () => {
-    return (
-        <section id="education-section" className="portfolio-block cv">
-            <div className="container">
-                <div className="education group">
-                    <div className="heading">
-                        <h2 className="text-center education-heading">Education</h2>
-                    </div>
-                    <div className="item">
-                        <div className="row">
-                            <div className="col-md-6">
-                                <h3 className="role">Bachelors of Engineering, Electronics and Communication</h3>
-                                <h4 className="organization">SSN College of Engineering</h4>
-                            </div>
-                            <div className="col-6 custom-period"><span className="period">2016 - 2020</span></div>
-                        </div>
-                        <p className="text-muted role-description">• Coursework: Networking, FPGA Programming, Digital Electronics, Digital
-                        Signal Processing, Antennas and Wave Propagation.<br /> <br /> • CGPA: 7.4 / 10</p>
-                    </div>
-                    <div className="item">
-                        <div className="row">
-                            <div className="col-md-6">
-                                <h3 className="role">Science Group</h3>
-                                <h4 className="organization">Holy Cross School</h4>
-                            </div>
-                            <div className="col-md-6 custom-period"><span className="period">2003 - 2016</span></div>
-                        </div>
-                        <p className="text-muted role-description">• Coursework: Biology, Maths, Physics, Chemistry. <br /><br />• Percentage: 94%</p>
-                    </div>
-                </div>
+  return (
+    <section id="education-section" className="portfolio-block cv">
+      <div className="container">
+        <div className="education group">
+          <div className="heading">
+            <h2 className="text-center education-heading">Education</h2>
+          </div>
+          <div className="item">
+            <div className="row">
+              <div className="col-md-6">
+                <h3 className="role">
+                  Bachelors of Engineering, Electronics and Communication
+                </h3>
+                <h4 className="organization">SSN College of Engineering</h4>
+              </div>
+              <div className="col-6 custom-period">
+                <span className="period">2016 - 2020</span>
+              </div>
             </div>
-        </section>
-    );
-}
+            <p className="role-description">
+              •{" "}
+              <span style={{ letterSpacing: "0.7px" }} className="bold">
+                Coursework
+              </span>
+              : Networking, FPGA Programming, Digital Electronics, Digital
+              Signal Processing, Antennas and Wave Propagation.
+            </p>
+          </div>
+          <div className="item">
+            <div className="row">
+              <div className="col-md-6">
+                <h3 className="role">Science Group</h3>
+                <h4 className="organization">Holy Cross School</h4>
+              </div>
+              <div className="col-md-6 custom-period">
+                <span className="period">2003 - 2016</span>
+              </div>
+            </div>
+            <p className="role-description">
+              •{" "}
+              <span style={{ letterSpacing: "0.7px" }} className="bold">
+                Coursework
+              </span>
+              : Biology, Maths, Physics, Chemistry.
+            </p>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
 
 export default Education;
--- src/components/Experience/Experience.js
+++ src/components/Experience/Experience.js
@@ -3,70 +3,103 @@ import "./Experience.css";
 import "../../assets/bootstrap/css/bootstrap.min.css";
 
 const Experience = () => {
-    return (
-        <section id="experience-section" className="portfolio-block cv">
-            <div className="container">
-                <div className="work-experience group">
-                    <div className="heading">
-                        <h2 className="text-center experience-heading">Experience</h2>
-                    </div>
-                    <div className="item">
-                        <div className="row">
-                            <div className="col-md-6">
-                                <h3 className="role">Software Developer</h3>
-                                <h4 className="organization">CargoAi - Singapore</h4>
-                            </div>
-                            <div className="col-md-6 custom-period"><span className="period">Aug 2020 - Present</span></div>
-                        </div>
-                        <p className="text-muted role-description">• Developed CO2 Shipment tracker micro service using Go and Python which is used by 1000's of users around the world. <br /> <br />
-• Developed flight tracker micro service using Go and Python which is used by 1000's of users around the world. <br /> <br />
-• Developed company's website using React.</p>
-                    </div>
-                    <div className="item">
-                        <div className="row">
-                            <div className="col-md-6">
-                                <h3 className="role">Software Developer - Intern</h3>
-                                <h4 className="organization">Qube Cinema - Chennai</h4>
-                            </div>
-                            <div className="col-md-6 custom-period"><span className="period">May 2019 - Jul 2020</span></div>
-                        </div>
-                        <p className="text-muted role-description">• Worked in the development of next generation Playback Server for
-                            theatres which will be used in 4000 screens across the world. <br /> <br />
-                                • Worked in the development of Automation Controller which automates the manual tasks done
-                            in theatres. <br /> <br />
-                                    • Worked in the migration of a classical service into Server-less Architecture and deployed
-                            it in AWS Lambda which will be used by thousands of distributors.</p>
-                    </div>
-                    <div className="item">
-                        <div className="row">
-                            <div className="col-md-6">
-                                <h3 className="role">Software Developer - Intern</h3>
-                                <h4 className="organization">Qube Cinema - Chennai</h4>
-                            </div>
-                            <div className="col-md-6 custom-period"><span className="period">Aug 2018 - Dec 2018</span></div>
-                        </div>
-                        <p className="text-muted role-description">• Designed and developed an app which generates an XML ﬁle from the
-                        available cinema assets. It is used for publishing the content to iTunes.
-                            <br /> <br />
-                                            • Designed and developed a command line application which monitors the status of hard drives
-                            currently in use and store the logs in a ﬁle.</p>
-                    </div>
-                    <div className="item">
-                        <div className="row">
-                            <div className="col-md-6">
-                                <h3 className="role">Research Analyst - Intern</h3>
-                                <h4 className="organization">BotAroma</h4>
-                            </div>
-                            <div className="col-md-6 custom-period"><span className="period">Dec 2017</span></div>
-                        </div>
-                        <p className="text-muted role-description">• Trained Chatbots using deep learning. <br /> <br />
-                                                • Collected and generated numerous resources for training the chatbot in order to achieve
-                            greater efficiency.</p>
-                    </div>
-                </div>
+  return (
+    <section id="experience-section" className="portfolio-block cv">
+      <div className="container">
+        <div className="work-experience group">
+          <div className="heading">
+            <h2 className="text-center experience-heading">Experience</h2>
+          </div>
+          <div className="item">
+            <div className="row">
+              <div className="col-md-6">
+                <h3 className="role">Software Engineer</h3>
+                <h4 className="organization">CargoAi - Singapore</h4>
+              </div>
+              <div className="col-md-6 custom-period">
+                <span className="period">Aug 2020 - Present</span>
+              </div>
             </div>
-        </section>
-    )
-}
+            <p className="role-description">
+              •{" "}
+              <span style={{ letterSpacing: "0.7px" }} className="bold">
+                Subscription Platform
+              </span>
+              : Architected and built a subscription platform for all the
+              services offered by CargoAi. Integrated with RapidAPI to
+              effortlessly subscribe and use the API.
+              <br />
+              <br />• <span style={{ letterSpacing: "0.7px" }} className="bold">Track and Trace</span>: Designed
+              and developed an extensible end-to-end Track & Trace service for
+              several major Airlines (Air France, British Airways, Etihad,
+              Finnair, etc.) integrated with our application. 1000’s of cargo
+              forwarders use this service for day-to-day tracking of their
+              shipments. <br />
+              <br />• <span style={{ letterSpacing: "0.7px" }} className="bold">Carbon Emission Calculator</span>:
+              Designed and developed a 99% accurate carbon emission calculator
+              service for air cargo shipments transported with various carriers.
+              Both airlines and forwarders use this service extensively to
+              calculate carbon emission for shipments and transport it
+              eco-friendly.
+              <br />
+              <br />• <span style={{ letterSpacing: "0.7px" }} className="bold">Flight Schedule</span>: Implemented
+              flight schedule service to get real-time schedules of all cargo
+              airlines with the market rate. Helped forwarders to choose the
+              best cost-effective carrier for their shipment. <br />
+              <br />• <span style={{ letterSpacing: "0.7px" }} className="bold">Website</span>: Designed and
+              developed the company’s website with responsive design and
+              excellent search engine optimization using GatsbyJS.
+            </p>
+          </div>
+          <div className="item">
+            <div className="row">
+              <div className="col-md-6">
+                <h3 className="role">Software Engineer - Intern</h3>
+                <h4 className="organization">Qube Cinema - Chennai</h4>
+              </div>
+              <div className="col-md-6 custom-period">
+                <span className="period">May 2019 - Jul 2020</span>
+              </div>
+            </div>
+            <p className="role-description">
+              • <span style={{ letterSpacing: "0.7px" }} className="bold">XP 4.0</span>: Worked in the development
+              of DCI certified next-generation digital cinema server that
+              enables movie playback in more than 4500 screens around the world.{" "}
+              <br />
+              <br />• <span style={{ letterSpacing: "0.7px" }} className="bold">Automation Controller</span>:
+              Worked in the development of Automation Controller, which
+              automates around 90% of the manual tasks done when playing movies
+              in theatres. It can be easily integrated with theatre management
+              system for much more automation. <br />
+              <br /> • <span style={{ letterSpacing: "0.7px" }} className="bold">Distributor Asset Manager</span>:
+              Designed and developed distributor asset management service which
+              helps distributor’s to manage their assets in the cloud.
+            </p>
+          </div>
+          <div className="item">
+            <div className="row">
+              <div className="col-md-6">
+                <h3 className="role">Software Engineer - Intern</h3>
+                <h4 className="organization">Qube Cinema - Chennai</h4>
+              </div>
+              <div className="col-md-6 custom-period">
+                <span className="period">Aug 2018 - Dec 2018</span>
+              </div>
+            </div>
+            <p className="role-description">
+              • <span style={{ letterSpacing: "0.7px" }} className="bold">iTunes Publisher</span>: Developed an
+              internal iTunes publisher tool that restructures and publishes
+              digital cinema contents to iTunes. <br />
+              <br />•<span style={{ letterSpacing: "0.7px" }} className="bold"> HDD health monitor</span>:
+              Developed an HDD health monitor service that monitors the health
+              of the hard disk drives currently in use and sends the periodic
+              reports in slack.
+            </p>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
 
-export default Experience
+export default Experience;
